5a06b0421d5fd273b96f8691075811712e8fe6af
CorreaBrunoMiguel <devsistemascorrea@gmail.com>
2024-02-09 16:38:40 -0300

    Criado construtor padrão (String nome, int idade) e um construtor sobrecarregado (string nome, int idade, int numeroTentativas. Criado getter e setter para dadosdo jogador (nome, idade)

--- src/Jogador.java
+++ src/Jogador.java
@@ -3,7 +3,42 @@ public class Jogador {
     private String nome;
     private int idade;
     private int pontuacao = 0;
-    private int numeroTentaivas = 0;
+    private int numeroTentativas = 0;
+
+    public Jogador(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public Jogador(String nome, int idade, int numeroTentativas) {
+        this.nome = nome;
+        this.idade = idade;
+        this.numeroTentativas = numeroTentativas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getNumeroTentativas() {
+        return numeroTentativas;
+    }
+
+    public void setNumeroTentativas(int numeroTentativas) {
+        this.numeroTentativas = numeroTentativas;
+    }
 
     public void adicionarPontos(int pts) {
         this.pontuacao += pts;
@@ -14,6 +49,6 @@ public class Jogador {
     }
 
     public void adicionaTentativa(int quantidade) {
-        this.numeroTentaivas += quantidade;
+        this.numeroTentativas += quantidade;
     }
 }
